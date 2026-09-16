0c2823ebe6dd99d4babd69cd559a81df73b593c0
Patryk Goraj <Patryk Goraj>
2026-09-16 14:00:03 +0200

    dodanie projektu

---
+++ Zadania01/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Podaj imię:  ");
+    String imie = scanner.nextLine();
+
+    System.out.println("Podaj wiek:  ");
+    String wiek = scanner.nextLine();
+}
